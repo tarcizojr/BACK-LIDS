289ddbb4c123188b26b970fcf4c98d0fc105f57b
Thallyta <thallytamedeiros2011@gmail.com>
2023-08-24 23:58:35 -0300

    Swagger - documentação do cadastro de projetos

--- lids-back-main/src/main/java/br/edu/ifpb/lids/presentation/control/ProjetoController.java
+++ lids-back-main/src/main/java/br/edu/ifpb/lids/presentation/control/ProjetoController.java
@@ -4,12 +4,15 @@ import br.edu.ifpb.lids.business.service.ProjetoService;
 import br.edu.ifpb.lids.business.service.impl.ConverteService;
 import br.edu.ifpb.lids.model.entity.Projeto;
 import br.edu.ifpb.lids.presentation.dto.ProjetoDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Api(value = "PROJETO")
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/projeto")
@@ -24,6 +27,7 @@ public class ProjetoController {
     @Autowired
 	private ModelMapper mapper;
 
+    @ApiOperation(value = "Cadastra projeto")
     @PostMapping
     public ResponseEntity create(@RequestBody ProjetoDto dto) {
 
--- lids-back-main/src/main/java/br/edu/ifpb/lids/presentation/dto/ProjetoDto.java
+++ lids-back-main/src/main/java/br/edu/ifpb/lids/presentation/dto/ProjetoDto.java
@@ -1,5 +1,6 @@
 package br.edu.ifpb.lids.presentation.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -8,20 +9,28 @@ import java.util.List;
 @Data
 public class ProjetoDto {
 
+    @ApiModelProperty(value = "Identificador do Projeto.",example = "1")
     private Long id;
 
+    @ApiModelProperty(value = "Título do Projeto.",example = "Projelógica: a ciência da computação nas escolas públicas de Monteiro-PB.")
     private String titulo;
 
+    @ApiModelProperty(value = "Tipo do Projeto.",example = "EXTENSAO")
     private String tipo;
 
+    @ApiModelProperty(value = "Breve descrição do Projeto.",example = "Promover a melhoria na formação e no desempenho dos estudantes do 9º ano")
     private String descricao;
 
+    @ApiModelProperty(value = "Data de início da execução.",example = "2023-08-25")
     private Date dataInicio;
 
+    @ApiModelProperty(value = "Data de início da execução.",example = "2024-01-01")
     private Date dataTermino;
 
+    @ApiModelProperty(value = "Status do Projeto.",example = "ATIVO")
     private String status;
 
+    @ApiModelProperty(value = "Lista de Colaboradores que estão no projeto.")
     private List<ColaboradorDto> colaboradores;
 
 }
