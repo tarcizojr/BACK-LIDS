4983bc23b602777e1dee272514bb548f3332106a
Guilherme Rodrigues <guilherme.ads.2022@gmail.com>
2023-08-31 21:41:32 -0300

    Adiciona TestCadastroProjetos

---
+++ lids-back-main/src/test/java/br/edu/ifpb/lids/TestesAutomatizados/TestCadastroProjeto.java
@@ -0,0 +1,70 @@
+package br.edu.ifpb.lids.TestesAutomatizados;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCadastroProjeto {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    public void timeSleep() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void cadastrodeProjeto() {
+        driver.get("http://localhost:3000/");
+        driver.manage().window().setSize(new Dimension(1920, 1080));
+
+        String urlListarColaboradores = driver.getCurrentUrl();
+        assertEquals(urlListarColaboradores, "http://localhost:3000/colaboradores");
+
+        driver.findElement(By.cssSelector("a:nth-child(1) .p-button-label")).click();
+
+        String urlListarProjeto = driver.getCurrentUrl();
+        assertEquals(urlListarProjeto, "http://localhost:3000/projetos");
+
+        driver.findElement(By.cssSelector(".p-button-raised")).click();
+
+        String urlCriarProjeto = driver.getCurrentUrl();
+        assertEquals(urlCriarProjeto, "http://localhost:3000/projetos");
+
+        driver.findElement(By.id("nomeDoProjeto")).click();
+        driver.findElement(By.id("nomeDoProjeto")).sendKeys("Projeto Fenix");
+        driver.findElement(By.cssSelector(".p-dropdown-trigger")).click();
+        driver.findElement(By.cssSelector(".p-dropdown-item:nth-child(2)")).click();
+        driver.findElement(By.id("objetivo")).click();
+        driver.findElement(By.id("objetivo")).sendKeys(
+                "Lorem ipsum dolor sit amet. A optio veritatis ut voluptatem galisum et maiores rerum et vero adipisci ut asperiores dolores. Sit laborum voluptatem ut molestiae tempora ad explicabo aliquam ut voluptate laboriosam.");
+        driver.findElement(By.id("dataInicio")).click();
+        driver.findElement(By.id("dataInicio")).sendKeys("09042020");
+        driver.findElement(By.id("dataFim")).click();
+        driver.findElement(By.id("dataFim")).sendKeys("09042023");
+        driver.findElement(By.cssSelector(".p-button-raised > .p-button-label")).click();
+        WebElement cardValid = driver.findElement(By.className("p-toast-summary"));
+        assertEquals(cardValid.getText(), "Confirmado");
+
+    }
+}
--- lids-back-main/src/test/java/br/edu/ifpb/lids/TestesAutomatizados/TestEditarColaborador.java
+++ lids-back-main/src/test/java/br/edu/ifpb/lids/TestesAutomatizados/TestEditarColaborador.java
@@ -10,14 +10,13 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestEditarColaborador {
 
-    private WebDriver driver;
- 
-    @Before
-    public void setUp() {
+  private WebDriver driver;
+
+  @Before
+  public void setUp() {
     System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
     driver = new ChromeDriver();
   }
@@ -26,49 +25,45 @@ public class TestEditarColaborador {
   public void tearDown() {
     driver.quit();
   }
-  
-  public void timeSleep(){
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
 
-    @Test
-    public void editarColaboradorSucesso () {
-        driver.get("http://localhost:3000/");
-        driver.manage().window().setSize(new Dimension(1920, 1080));
-
-        WebDriverWait wait = new WebDriverWait(driver, 20); 
-
-        driver.findElement(By.cssSelector("a:nth-child(2) .p-button-label")).click();
-
-        String urlListarColaboradores = driver.getCurrentUrl();  
-        assertEquals(urlListarColaboradores, "http://localhost:3000/colaboradores");
-
-        driver.findElement(By.cssSelector(".card-butons > .p-button:nth-child(1)")).click();
-
-        driver.findElement(By.id("nome")).click();
-        driver.findElement(By.id("nome")).sendKeys("Guilherme Rodrigues");
-        driver.findElement(By.id("dataNascimento")).click();
-        driver.findElement(By.id("dataNascimento")).sendKeys("09042002");
-        timeSleep();
-        driver.findElement(By.cssSelector(".p-button-raised > .p-button-label")).click();
-        driver.findElement(By.cssSelector(".p-confirm-dialog-accept")).click();
-        WebElement cardValid = driver.findElement(By.className("p-toast-summary"));
-        assertEquals(cardValid.getText(), "Confirmado");
-        try {
-          Thread.sleep(1000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
-        WebElement cardValidSucess = driver.findElement(By.className("p-toast-summary"));
-        assertEquals(cardValidSucess.getText(), "Sucesso");
-        timeSleep();
-
-
-        
+  public void timeSleep() {
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
-    
+  }
+
+  @Test
+  public void editarColaboradorSucesso() {
+    driver.get("http://localhost:3000/");
+    driver.manage().window().setSize(new Dimension(1920, 1080));
+
+    driver.findElement(By.cssSelector("a:nth-child(2) .p-button-label")).click();
+
+    String urlListarColaboradores = driver.getCurrentUrl();
+    assertEquals(urlListarColaboradores, "http://localhost:3000/colaboradores");
+
+    driver.findElement(By.cssSelector(".card-butons > .p-button:nth-child(1)")).click();
+
+    driver.findElement(By.id("nome")).click();
+    driver.findElement(By.id("nome")).sendKeys("Guilherme Rodrigues");
+    driver.findElement(By.id("dataNascimento")).click();
+    driver.findElement(By.id("dataNascimento")).sendKeys("09042002");
+    timeSleep();
+    driver.findElement(By.cssSelector(".p-button-raised > .p-button-label")).click();
+    driver.findElement(By.cssSelector(".p-confirm-dialog-accept")).click();
+    WebElement cardValid = driver.findElement(By.className("p-toast-summary"));
+    assertEquals(cardValid.getText(), "Confirmado");
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    WebElement cardValidSucess = driver.findElement(By.className("p-toast-summary"));
+    assertEquals(cardValidSucess.getText(), "Sucesso");
+    timeSleep();
+
+  }
+
 }
