d27922af73eea69761890bd46ab26ab2c9ec3367
tarcizo <tarcizoleitemonteiro@gmail.com>
2023-09-14 21:28:23 -0300

    cors solunionado

--- lids-back-main/src/main/java/br/edu/ifpb/lids/config/SecurityConfig.java
+++ lids-back-main/src/main/java/br/edu/ifpb/lids/config/SecurityConfig.java
@@ -1,6 +1,10 @@
 package br.edu.ifpb.lids.config;
 
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +12,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.core.Ordered;
 
 @Configuration
 @EnableWebSecurity
@@ -30,4 +39,25 @@ public class SecurityConfig {
         .build();
   }
 
+  	@Bean
+	  public FilterRegistrationBean<CorsFilter> corsFilter() {
+
+		List<String> all = Arrays.asList("*");
+
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowedMethods(all);
+		corsConfiguration.setAllowedOriginPatterns(all);
+		corsConfiguration.setAllowedHeaders(all);
+		corsConfiguration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+
+		CorsFilter corFilter = new CorsFilter(source);
+
+		FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<CorsFilter>(corFilter);
+		filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+		return filter;
+	}
 }
--- lids-back-main/src/main/java/br/edu/ifpb/lids/presentation/control/ProjetoController.java
+++ lids-back-main/src/main/java/br/edu/ifpb/lids/presentation/control/ProjetoController.java
@@ -20,7 +20,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
-@CrossOrigin("*")
+
 @RestController
 @RequestMapping("/api/projeto")
 public class ProjetoController {
